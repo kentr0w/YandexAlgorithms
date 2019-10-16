4736092bc2eb0beeb7d68ea0aa1c2a5904488758
kentr0w <qbextted0@gmail.com>
2019-10-16 15:36:05 +0300

    fdv

--- SecondContest/Fourth/main.cpp
+++ SecondContest/Fourth/main.cpp
@@ -1,73 +1,60 @@
 #include <iostream>
-#include <vector>
+using namespace std;
 
-unsigned int cur = 0;
+#define N 3
 
-unsigned int nextRand24(unsigned long long firstElement, unsigned long long secondElement) {
-    cur = static_cast<unsigned int>(cur * firstElement + secondElement);
-    return cur >> 8;
-}
+// Partition using Hoare's Partitioning scheme
+int Partition(int a[], int low, int high)
+{
+    int pivot = a[1];
+    int i = low - 1;
+    int j = high + 1;
+    while(1)
+    {
+        do {
+            i++;
+        } while (a[i] < pivot);
 
-unsigned int nextRand32(unsigned long long firstElement, unsigned long long secondElement) {
-    unsigned int firstvar = nextRand24(firstElement, secondElement),
-            secondvar = nextRand24(firstElement, secondElement);
-    return (firstvar << 8) ^ secondvar;
-}
+        do {
+            j--;
+        } while (a[j] > pivot);
 
+        if(i >= j)
+            return j;
 
-size_t Partition(std::vector<unsigned int>& data, size_t leftSide, size_t rigthSide) {
-    size_t middle = (data.size() / 2);
-    std::swap(data[middle], data[rigthSide]);
-    size_t left = leftSide, right = rigthSide;
-    while (left <= right) {
-        while (data[left] < data[middle] && left <= right) {
-            ++left;
-        }
-
-        while (data[right] > data[middle] && left <= right) {
-            --right;
-        }
-        if(left <= right) {
-            std::swap(data[left], data[right]);
-            ++left;
-            --right;
-        }
+        swap(a[i], a[j]);
     }
-    return right;
 }
 
-int Qselect(std::vector<unsigned int> &data, size_t leftSide, size_t rigthSide){
-    if(rigthSide - leftSide == 0)
-        return data[rigthSide];
-    size_t order = Partition(data, leftSide, rigthSide);
-    if( order < data.size()/2){
-        return Qselect(data, order, rigthSide);
-    }
-    else if(order > data.size()/2){
-        return Qselect(data, leftSide, order);
-    }
-    else
-        return data[order];
+// Quicksort routine
+void QuickSort(int a[], int low, int high)
+{
+    // base condition
+    if(low >= high)
+        return;
+
+    // rearrange the elements across pivot
+    int pivot = Partition(a, low, high);
+
+    std::cout<<pivot<<"\n";
+    // recur on sub-array containing elements that are less than pivot
+    QuickSort(a, low, pivot);
+
+    // recur on sub-array containing elements that are more than pivot
+    QuickSort(a, pivot + 1, high);
 }
 
+int main()
+{
+    int arr[] = {3,1,2};
+    srand(time(NULL));
 
+    // generate random input of integers
 
+    QuickSort(arr, 0, N - 1);
 
-int main() {
-    /*std::ios_base::sync_with_stdio(false);
-    size_t count;
-    unsigned long long firstElement, secondElement;
-    std::cin >> count;
-    std::cin >> firstElement >> secondElement;
-    std::vector<unsigned int> data;
-    data.reserve(count);
-    for (size_t index = 0; index < count; ++index) {
-        data.push_back(nextRand32(firstElement, secondElement));
-    }*/
-    //{2,1,7,3,3};
-    //{1,1,1,1};
-    std::vector<unsigned int> data = {1,1,1,1};
-    std::cout<<Qselect(data, 0, data.size()-1);
+    for (int i = 0 ; i < N; i++)
+        cout << arr[i] << " ";
+
     return 0;
 }
-
