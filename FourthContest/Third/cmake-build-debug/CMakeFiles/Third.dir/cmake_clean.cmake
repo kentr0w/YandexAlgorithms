b2938fb3bf160e144519d86000a7089ba5545646
kentr0w <qbextted0@gmail.com>
2019-11-08 22:08:12 +0300

    done, but TL

--- FourthContest/Third/cmake-build-debug/CMakeFiles/Third.dir/cmake_clean.cmake
+++ FourthContest/Third/cmake-build-debug/CMakeFiles/Third.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Third.dir/main.cpp.o"
-  "Third.pdb"
   "Third"
+  "Third.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
--- FourthContest/Third/main.cpp
+++ FourthContest/Third/main.cpp
@@ -3,83 +3,72 @@
 
 
 struct Vertex {
-    int a;
     std::vector<int> next;
     std::vector<int> capacity;
     bool visited;
 };
 
 bool isCorrect(int first, int second, int first_kik, int second_kik) {
-
-    if((first == first_kik && second == second_kik) || (first == second_kik && second_kik == second))
-        return false;
-
-    return true;
+    return !((first == first_kik && second == second_kik) ||
+    (first == second_kik && second_kik == second));
 }
 
-bool BFS(std::vector<Vertex> &graph, int first_kik, int second_kik, int& count, int current) {
-
-    std::cout<<"curent = " << current + 1 << "count = " << count<<"\n";
-
-    if(count == graph.size() - 1)
+bool BFS(std::vector<Vertex> &graph, int first_kik, int second_kik,
+        unsigned long &count, int current) {
+    if (count == graph.size() - 1)
         return false;
-
-
     std::vector<int> nextvertex;
-
-    for(int i=0; i<graph[current].next.size(); ++i){
-        if(isCorrect(current, graph[current].next[i], first_kik, second_kik) && !graph[graph[current].next[i]].visited){
-            graph[graph[current].next[i]].visited = true;
-            //++count;
-            nextvertex.push_back(graph[current].next[i]);
+    for (size_t index = 0; index < graph[current].next.size(); ++index) {
+        if (isCorrect(current, graph[current].next[index], first_kik, second_kik) &&
+            !graph[graph[current].next[index]].visited) {
+            graph[graph[current].next[index]].visited = true;
+            nextvertex.push_back(graph[current].next[index]);
         }
     }
-
-    for(int i=0; i < nextvertex.size(); ++i){
-        if(!BFS(graph, first_kik, second_kik, ++count, nextvertex[i]))
+    for (int element : nextvertex) {
+        if (!BFS(graph, first_kik, second_kik, ++count, element))
             return false;
     }
-
     return true;
 }
 
-
 void clean(std::vector<Vertex> &graph) {
-    for (auto &i : graph)
-        i.visited = false;
+    for (auto &element : graph)
+        element.visited = false;
 }
 
-void Solve(std::vector<Vertex> &graph,  std::vector<std::vector<int>> data) {
+void Solve(std::vector<Vertex> &graph, std::vector<std::vector<int>> data) {
 
     int max = INT32_MAX;
-    for(int i=0; i<data.size(); ++i){
-        graph[data[i][0]].visited = true;
-        int count = 0;
-        if(BFS(graph, data[i][0], data[i][1], count, data[i][0])){
-            if(data[i][2] < max)
-                max = data[i][2] + 1;
-            std::cout<<"now is "<< data[i][0] + 1<< " "<<data[i][1] + 1<<"\n";
+    bool isChange = false;
+    for (size_t index = 0; index < data.size(); ++index) {
+        graph[data[index][0]].visited = true;
+        unsigned long count = 0;
+        if (BFS(graph, data[index][0], data[index][1], count, data[index][0])) {
+            if (data[index][2] < max) {
+                max = data[index][2] + 1;
+                isChange = true;
+            }
         }
         clean(graph);
     }
-
-    std::cout<<"SOLVE = "<<max;
+    isChange ? std::cout << max : std::cout << -1;
 }
 
 int ReadNum() {
-    int x;
-    std::cin >> x;
-    return x;
+    int num;
+    std::cin >> num;
+    return num;
 }
 
-std::vector<std::vector<int>> ReadGraph(int m) {
+std::vector<std::vector<int>> ReadGraph(int ways) {
 
-    std::vector<std::vector<int>> data(m, std::vector<int>(3));
+    std::vector<std::vector<int>> data(ways, std::vector<int>(3));
 
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < ways; ++i) {
         for (int j = 0; j < 3; ++j) {
-            int x = ReadNum();
-            data[i][j] = x - 1;
+            int num = ReadNum();
+            data[i][j] = num - 1;
         }
     }
     return data;
@@ -94,41 +83,21 @@ int main() {
 
     std::vector<Vertex> graph(citis);
 
-    for (int i = 0; i < citis; ++i) {
+
+    for (int index = 0; index < citis; ++index) {
         Vertex vertex = Vertex();
         vertex.visited = false;
-        vertex.a = i;
-        graph[i] = vertex;
+        graph[index] = vertex;
     }
 
-    for (int i = 0; i < ways; ++i) {
-
-        graph[data[i][0]].next.push_back(data[i][1]);
-        graph[data[i][0]].capacity.push_back(data[i][2]);
-
-        graph[data[i][1]].next.push_back(data[i][0]);
-        graph[data[i][1]].capacity.push_back(data[i][2]);
-
+    for (int index = 0; index < ways; ++index) {
+        graph[data[index][0]].next.push_back(data[index][1]);
+        graph[data[index][0]].capacity.push_back(data[index][2]);
+        graph[data[index][1]].next.push_back(data[index][0]);
+        graph[data[index][1]].capacity.push_back(data[index][2]);
     }
-
-    std::cout << "\n";
-
-    for (int i = 0; i < citis; ++i) {
-        for (int j = 0; j < graph[i].next.size(); ++j)
-            std::cout << "from = " << graph[i].a + 1 << " to " << graph[i].next[j] + 1 << " cap = "
-                      << graph[i].capacity[j] + 1 << "\n";
-        std::cout << "\n";
-    }
-
-    std::cout << "\n";
-    std::cout << "\n";
-    std::cout << "\n";
 
     Solve(graph, data);
-
-    /*graph[data[2][0]].visited = true;
-    int count = 0;
-    std::cout<<BFS(graph, data[2][0], data[2][1], count, data[2][0]);*/
 
     return 0;
 }
